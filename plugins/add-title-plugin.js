958caac48978036a3966173b8b48b3ee7c1b8a81
liuduan <liuduan.05.05@163.com>
2018-05-23 20:42:04 +0800

    bugfix

--- plugins/add-title-plugin.js
+++ plugins/add-title-plugin.js
@@ -3,13 +3,20 @@ class AddTitlePlugin {
     apply(compiler) {
         compiler.plugin('make', function (compilation, callback) {
             compilation.plugin("optimize-chunk-assets", function (chunks, cb) {
-                chunks.forEach(chunk => {
-                    chunk.files.forEach(function (file) {
-                        if (/.+(js)$/.test(file)) {
-                            compilation.assets[file]._value = '/**liuduan **/\n' + compilation.assets[file]._value;
-                        }
+                try {
+                    chunks.forEach(chunk => {
+                        chunk.files.forEach(function (file) {
+                            if (/.+(js)$/.test(file)) {
+                                if (typeof compilation.assets[file]._value === 'string')
+                                    compilation.assets[file]._value = '/**liuduan **/\n' + compilation.assets[file]._value;
+                                else
+                                    compilation.assets[file]._source.children.unshift('/**liuduan **/\n')
+                            }
+                        });
                     });
-                });
+                } catch (error) {
+                    console.log(error);
+                }
                 cb();
             });
             callback();
